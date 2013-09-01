8991bdaeff0ea08cd097a3340a9e2e05761c39d7
1n5aN1aC <joshuavillwo@gmail.com>
2013-08-31 21:08:56 -0700

    Added Initilization

--- common/tacotek.java
+++ common/tacotek.java
@@ -1,6 +1,8 @@
 package assets.tacotek.common;
 
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.Init;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.network.NetworkMod;
 
 @Mod(modid = tacotek.modid, name = "TacoTek", version = "0.1")
@@ -8,5 +10,10 @@ import cpw.mods.fml.common.network.NetworkMod;
 public class tacotek {
 	
 	public static final String modid = "tacotek";
+	
+	@Init
+	public void load(FMLInitializationEvent event) {
+		
+	}
 	
 }
