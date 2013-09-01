4c9cbd2236bdc7055f552b892383a488548c97a0
1n5aN1aC <joshuavillwo@gmail.com>
2013-08-31 20:46:31 -0700

    Changed TakoTek to TacoTek

--- common/tacotek.java
+++ common/tacotek.java
@@ -3,7 +3,7 @@ package assets.tacotek.common;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.network.NetworkMod;
 
-@Mod(modid = tacotek.modid, name = "TakoTek", version = "0.1")
+@Mod(modid = tacotek.modid, name = "TacoTek", version = "0.1")
 @NetworkMod(clientSideRequired = true, serverSideRequired = true)
 public class tacotek {
 	
