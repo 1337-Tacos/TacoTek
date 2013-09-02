f29eace0d3f8ed885d52340fd204cf2f8cc35915
1n5aN1aC <joshuavillwo@gmail.com>
2013-09-01 23:12:14 -0700

    Added Custom Textures To taco Block

--- blocks/BlockTaco.java
+++ blocks/BlockTaco.java
@@ -6,14 +6,17 @@ import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IconRegister;
+import net.minecraft.util.Icon;
 
-public class BlockTaco extends Block
-{
+public class BlockTaco extends Block {
 
 	private static String textureName;
 	
-	public BlockTaco(int id, String textureName)
-	{
+	@SideOnly(Side.CLIENT)
+	private Icon topTexture;
+	private Icon bottomTexture;
+	
+	public BlockTaco(int id, String textureName) {
 		super(id, Material.rock);
 		
 		this.textureName = textureName;
@@ -22,5 +25,18 @@ public class BlockTaco extends Block
 	@SideOnly(Side.CLIENT)
 	public void registerIcons(IconRegister reg) {
 		this.blockIcon = reg.registerIcon(tacotek.modID + ":" + textureName);
+		this.topTexture = reg.registerIcon(tacotek.modID + ":" + textureName + "_top");
+		this.bottomTexture = reg.registerIcon(tacotek.modID + ":" + textureName + "_bottom");
+	}
+	
+	@SideOnly(Side.CLIENT)
+	public Icon getIcon(int side, int meta) {
+		if(side == 1) {
+			return this.topTexture;
+		}
+		else if (side == 0) {
+			return this.bottomTexture;
+		}
+		return this.blockIcon;
 	}
 }
