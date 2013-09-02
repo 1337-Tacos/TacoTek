e4859cec9f9d04205f4a31214f2b579758da45d1
sulljason <sulljason@gmail.com>
2013-09-01 21:56:32 -0700

    Made Cheese Eatable

--- Items/Cheese.java
+++ Items/Cheese.java
@@ -3,15 +3,16 @@ package assets.tacotek.Items;
 import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemFood;
 import assets.tacotek.common.tacotek;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
-public class Cheese extends Item
+public class Cheese extends ItemFood
 {
 	public Cheese(int id)
 	{
-		super(id);
+		super(id, 2, false);
 		this.setCreativeTab(CreativeTabs.tabFood);
 	}
 	
