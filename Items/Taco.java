bb107ab639cf6659de57220d1f3ceb624bbd5d19
sulljason <sulljason@gmail.com>
2013-09-01 21:37:35 -0700

    Made Tacos Edable

--- Items/Taco.java
+++ Items/Taco.java
@@ -3,15 +3,16 @@ package assets.tacotek.Items;
 import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemFood;
 import assets.tacotek.common.tacotek;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
-public class Taco extends Item
+public class Taco extends ItemFood
 {
 	public Taco(int id)
 	{
-		super(id);
+		super(1, 30, true);
 		this.setCreativeTab(CreativeTabs.tabFood);
 	}
 	
