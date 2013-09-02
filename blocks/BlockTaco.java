fc5cd490a12a4a27baba76f0d5d190cb5396c549
1n5aN1aC <joshuavillwo@gmail.com>
2013-09-01 22:46:34 -0700

    Added The Worst Block Ever

--- blocks/BlockTaco.java
+++ blocks/BlockTaco.java
@@ -1,15 +1,26 @@
 package assets.tacotek.blocks;
 
+import assets.tacotek.common.tacotek;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IconRegister;
 
 public class BlockTaco extends Block
 {
 
-	public BlockTaco(int id)
+	private static String textureName;
+	
+	public BlockTaco(int id, String textureName)
 	{
 		super(id, Material.rock);
 		
+		this.textureName = textureName;
 	}
-
+	
+	@SideOnly(Side.CLIENT)
+	public void registerIcons(IconRegister reg) {
+		this.blockIcon = reg.registerIcon(tacotek.modID + ":" + textureName);
+	}
 }
