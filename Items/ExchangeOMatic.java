b616dec4ebd6fb7167aba1eefb198de5193222f8
1n5aN1aC <joshuavillwo@gmail.com>
2013-09-04 14:55:08 -0700

    Added Exchangeomatic

---
+++ Items/ExchangeOMatic.java
@@ -0,0 +1,45 @@
+package assets.tacotek.Items;
+
+import java.util.List;
+
+import net.minecraft.client.renderer.texture.IconRegister;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+import assets.tacotek.common.IDsHelper;
+import assets.tacotek.common.tacotek;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+
+public class ExchangeOMatic extends Item {
+
+	public ExchangeOMatic(int id)
+	{
+		super(id);
+		this.setCreativeTab(tacotek.tacotekTab);
+	}
+	
+	@SideOnly(Side.CLIENT)
+	public void registerIcons(IconRegister reg)
+	{
+		this.itemIcon = reg.registerIcon(tacotek.modID + ":" + this.getUnlocalizedName() );
+	}
+	
+	@SideOnly(Side.CLIENT)
+	public void addInformation(ItemStack itemStack, EntityPlayer player, List dataList, boolean bool) {
+		dataList.add("This is a placeholder");
+	}
+	
+	@Override
+	public boolean onItemUse(ItemStack stack, EntityPlayer player, World world, int x, int y, int z, int side, float par8, float par9, float par10) 
+	{
+		//Assuming player can edit that block, EDIT IT!
+		if (player.canPlayerEdit(x, y, z, side, stack)) {
+			world.setBlock(x, y, z, net.minecraft.block.Block.stone.blockID);
+			world.notifyBlockOfNeighborChange(x, y, z, net.minecraft.block.Block.stone.blockID);
+			return true;
+		}
+		else return false;
+	}
+}
