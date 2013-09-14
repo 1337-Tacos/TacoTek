cc247bf1b9fd31ccf9991296bba826389d2b1a35
sulljason <sulljason@gmail.com>
2013-09-14 00:03:33 -0700

    Fixed shield armor cast crash.
    If player was wearing any other chest besides shield armor game would
    crash.

--- common/TacoTickHandler.java
+++ common/TacoTickHandler.java
@@ -14,25 +14,29 @@ import cpw.mods.fml.common.network.Player;
 
 public class TacoTickHandler implements ITickHandler {
 	
-	private int currentInterval = 0;	//Tick counter.
-	private int armorRate = 100;		//Rate at which to Tick Armor
+	private int tCount=0;	//Tick counter.
+	private int armorRate=100;		//Rate at which to Tick Armor
 	
 	private void onPlayerTick(EntityPlayer player) {
-		currentInterval++;
+		tCount++;
 		
-		if (currentInterval == armorRate) {
+		if (tCount==armorRate) {
 			
 			//Zeroes out interval counter.
-			currentInterval = 0;
+			tCount=0;
 			
 			//If player is wearing chest armor.
-			if(player.getCurrentItemOrArmor(3) != null) {
+			if(player.getCurrentItemOrArmor(3)!=null) {
+				ItemStack stack=player.getCurrentItemOrArmor(3);
 				
-				ItemStack stack = player.getCurrentItemOrArmor(3);
-				ShieldArmor chest = (ShieldArmor) stack.getItem();
+				//If equiped item is a shield chest.
+				if (stack.getItem()==ItemsHelper.shield_chest) {
+					ShieldArmor chest = (ShieldArmor) stack.getItem();
 				
-				if (chest != null)
-					chest.tickArmor(stack, (Player) player);
+					if (chest!=null) {
+						chest.tickArmor(stack, (Player) player);
+					}
+				}
 			}
 		}
 	}
