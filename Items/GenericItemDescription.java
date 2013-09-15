2754842673bf6002e0c34840a6cfb1087cb2e3cb
sulljason <sulljason@gmail.com>
2013-09-15 14:13:07 -0700

    Added description to neutronium dust.

--- Items/GenericItemDescription.java
+++ Items/GenericItemDescription.java
@@ -1,10 +1,29 @@
 package assets.tacotek.Items;
 
-public class GenericItemDescription extends GenericItem {
+import java.util.List;
 
-	public GenericItemDescription(int id, String name, String desc, int stack) {
-		super(id, name, desc, stack);
-		// TODO Auto-generated constructor stub
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import cpw.mods.fml.relauncher.SideOnly;
+import cpw.mods.fml.relauncher.Side;
+
+public class GenericItemDescription extends GenericItem {
+	
+	String description="No description.";
+
+	public GenericItemDescription(int id, String name, String desc) {
+		super(id, name);
+		this.description=desc;
+	}
+	
+	public GenericItemDescription(int id, String name, String desc, int stack){
+		super(id, name, stack);
+		this.description=desc;
+	}
+
+	@SideOnly(Side.CLIENT)
+	public void addInformation(ItemStack itemStack, EntityPlayer player, List dataList, boolean bool) {
+		dataList.add(description);
 	}
 
 }
