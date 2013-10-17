5c92a7c980b3bed2a75707f8f07d0bb9b4d5b323
sulljason <sulljason@gmail.com>
2013-10-17 00:59:12 -0700

    Changed elect end chest desc
    Hole filler isn't functional yet.

--- Items/HoleFiller.java
+++ Items/HoleFiller.java
@@ -1,12 +1,10 @@
 package assets.tacotek.Items;
 
-import java.util.Arrays;
 import java.util.List;
 
 import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
-import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.inventory.InventoryEnderChest;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
@@ -14,11 +12,11 @@ import assets.tacotek.common.tacotek;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
-public class ElectricEnderChest extends GenericElectric {
+public class HoleFiller extends GenericElectric{
 
 	private static int energyCost = 10000;
 	
-	public ElectricEnderChest(int id, String name) {
+	public HoleFiller(int id, String name) {
 		super(id, 100000, 1, 100);
 		this.setUnlocalizedName(name);
 	}
@@ -30,23 +28,20 @@ public class ElectricEnderChest extends GenericElectric {
 	
 	@SideOnly(Side.CLIENT)
 	public void addInformation(ItemStack itemStack, EntityPlayer player, List dataList, boolean bool) {
-		dataList.add("A handheld enderchest. Harmful if swallowed.");
+		dataList.add("Fills creeper holes. Choking hazard. Contains small parts.");
 	}
 	
 	@Override
-	public ItemStack onItemRightClick(ItemStack stack, World par2World, EntityPlayer player) {
+	public boolean onItemUse(ItemStack stack, EntityPlayer player, World world, int x, int y, int z, int side, float par8, float par9, float par10) {
 		boolean isServer = player instanceof EntityPlayerMP;
 		
 		if (!canTakeDamage(stack, energyCost)) {
 			if (!isServer) {
 				player.addChatMessage("§8Not Enough Power!");
 			}
-			return stack;
+			return false;
 		}
 		
-		InventoryEnderChest endInv=player.getInventoryEnderChest();
-		damage(stack, energyCost, player);
-		player.displayGUIChest(endInv);
-		return stack;
+		return true;
 	}
 }
