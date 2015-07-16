cc14130a24ede3174e46cca9e2206bb8873d00e8
1n5aN1aC <joshuavillwo@gmail.com>
2015-07-15 19:05:32 -0700

    Nerfed Tacos slightly.

--- java/com/_1n5aN1aC/tacotek/items/Taco.java
+++ java/com/_1n5aN1aC/tacotek/items/Taco.java
@@ -14,8 +14,8 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 public class Taco extends GenericFood {
 	
 	public Taco(String name) {
-		super(name, 8, true);
-		this.setPotionEffect(22, 300, 0, 1.0F);
+		super(name, 8, 0.4f, true);
+		this.setPotionEffect(22, 240, 0, 1.0F);	//Absorption ,4 Minutes, 2 hearts, always
 		this.maxStackSize = 16;
 	}
 
