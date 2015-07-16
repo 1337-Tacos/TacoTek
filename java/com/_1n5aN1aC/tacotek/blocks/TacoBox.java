492d04c32338dc42e0c863e270e1bcb3c351d8a3
1n5aN1aC <joshuavillwo@gmail.com>
2015-07-15 17:49:12 -0700

    Made tacoBox drop 8-9 tacos, enabled textures

--- java/com/_1n5aN1aC/tacotek/blocks/TacoBox.java
+++ java/com/_1n5aN1aC/tacotek/blocks/TacoBox.java
@@ -5,7 +5,9 @@ import java.util.Random;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 
 import com._1n5aN1aC.tacotek.items.ItemsHelper;
@@ -22,13 +24,24 @@ public class TacoBox extends GenericBlock {
 		this.setStepSound(Block.soundTypeWood);
 	}
 	
-	public int quantityDropped(Random rand) {
-		return 9;
-	}
-	
 	@SideOnly(Side.CLIENT)
 	public void addInformation(ItemStack itemStack, EntityPlayer player, List dataList, boolean bool) {
 		dataList.add("Store a Lot of Tacos");
 		dataList.add("All in one Block.");
+	}
+	
+	@Override
+	public Item getItemDropped(IBlockState blockstate, Random random, int fortune) {
+	    return ItemsHelper.taco;
+	}
+
+	@Override
+	public int damageDropped(IBlockState blockstate) {
+	    return 0;
+	}
+
+	@Override
+	public int quantityDropped(IBlockState blockstate, int fortune, Random random) {
+	    return 8 + random.nextInt(2);
 	}
 }
