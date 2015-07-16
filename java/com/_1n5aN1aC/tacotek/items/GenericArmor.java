4d1dd4ddf134bd58b86accba2a212c7456d0e522
1n5aN1aC <joshuavillwo@gmail.com>
2015-07-15 19:36:05 -0700

    Added GenericArmor & Tuxedo

---
+++ java/com/_1n5aN1aC/tacotek/items/GenericArmor.java
@@ -0,0 +1,30 @@
+package com._1n5aN1aC.tacotek.items;
+
+import com._1n5aN1aC.tacotek.common.tacotek;
+
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemArmor;
+
+public class GenericArmor extends ItemArmor implements IRenderable {
+	
+	private final String name;
+	
+	public GenericArmor(String name, ArmorMaterial material, int renderIndex, int armorType) {
+		super(material, renderIndex, armorType);
+		this.name = name;
+		this.setUnlocalizedName(tacotek.MODID + "_" + name);
+		this.setCreativeTab(tacotek.tacotekTab);
+	}
+
+	/**
+	 * @return the itemName of the item
+	 */
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public Item getItem() {
+		return this;
+	}
+}
