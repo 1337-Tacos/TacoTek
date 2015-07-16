bb6471ae6750505292c68e84e975df0e3370530d
1n5aN1aC <joshuavillwo@gmail.com>
2015-07-16 13:09:58 -0700

    Remove old code

--- java/com/_1n5aN1aC/tacotek/proxy/ClientProxy.java
+++ java/com/_1n5aN1aC/tacotek/proxy/ClientProxy.java
@@ -1,59 +1,32 @@
 package com._1n5aN1aC.tacotek.proxy;
 
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.world.World;
-import net.minecraftforge.fml.client.FMLClientHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 import com._1n5aN1aC.tacotek.common.RenderRegistrationHelper;
 
+/**
+ * @author 1n5aN1aC
+ * Handles Proxying initialization events to the client, when ran on the clientside
+ */
 public class ClientProxy extends CommonProxy {
 	
 	@Override
-    public void preInit(FMLPreInitializationEvent e) {
-        super.preInit(e);
-    }
+	public void preInit(FMLPreInitializationEvent e) {
+		super.preInit(e);
+	}
 	
-    @Override
-    public void init(FMLInitializationEvent e) {
-        super.init(e);
-        
-        RenderRegistrationHelper.registerItemRenderer();
-        RenderRegistrationHelper.registerBlockRenderer();
-    }
-    
-    @Override
-    public void postInit(FMLPostInitializationEvent e) {
-        super.postInit(e);
-    }
-    
-    
+	@Override
+	public void init(FMLInitializationEvent e) {
+		super.init(e);
+		
+		RenderRegistrationHelper.registerItemRenderer();
+		RenderRegistrationHelper.registerBlockRenderer();
+	}
 	
-	public String getSide() {
-		return "Client";
-	}
-
-	public World getWorld() {
-		return FMLClientHandler.instance().getClient().theWorld;
-	}
-
-	public EntityPlayer getClientPlayer() {
-		return FMLClientHandler.instance().getClient().thePlayer;
-	}
-
-	public void tick() {
-		//Not Client Side
-	}
-
-	public Object getServerGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z) {
-		// Only Server-side
-		return null;
-	}
-
-	public Object getClientGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z) {
-		// TODO Auto-generated method stub
-		return null;
+	@Override
+	public void postInit(FMLPostInitializationEvent e) {
+		super.postInit(e);
 	}
 }
--- java/com/_1n5aN1aC/tacotek/proxy/ServerProxy.java
+++ java/com/_1n5aN1aC/tacotek/proxy/ServerProxy.java
@@ -1,14 +1,13 @@
 package com._1n5aN1aC.tacotek.proxy;
 
-import net.minecraft.client.multiplayer.WorldClient;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.world.World;
-import net.minecraft.world.WorldServer;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import net.minecraftforge.fml.server.FMLServerHandler;
 
+/**
+ * @author 1n5aN1aC
+ * Handles Proxying initialization events to the server, when ran on the serverside
+ */
 public class ServerProxy extends CommonProxy {
 	
 	@Override
@@ -25,62 +24,4 @@ public class ServerProxy extends CommonProxy {
     public void postInit(FMLPostInitializationEvent e) {
         super.postInit(e);
     }
-	
-	
-    
-    
-	
-	public String getSide() {
-		return "Server";
-	}
-
-	public World getWorld() {
-		return null;
-		//Unsure why, but LP does this.
-	}
-
-	public void registerTileEntities() {
-		//GameRegistry.registerTileEntity(LogisticsSolderingTileEntity.class, "logisticspipes.blocks.LogisticsSolderingTileEntity");
-	}
-
-	public EntityPlayer getClientPlayer() {
-		return null;
-		//Not Serverside
-	}
-
-	public boolean isMainThreadRunning() {
-		return FMLServerHandler.instance().getServer().isServerRunning();
-	}
-
-	public void registerParticles() {
-		//Only Client Side
-	}
-
-	public int addArmor(String armor) {
-		return 0;
-	}
-
-	public void tick() {
-		// TODO Auto-generated method stub
-	}
-
-	public int getDimensionForWorld(World world) {
-		if(world instanceof WorldServer) {
-			return ((WorldServer)world).provider.getDimensionId();
-		}
-		else if(world instanceof WorldClient) {
-			return ((WorldClient)world).provider.getDimensionId();
-		}
-		return 0;
-	}
-
-	public Object getServerGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public Object getClientGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z) {
-		// Only Client Side
-		return null;
-	}
 }
